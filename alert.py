81d7bc55aab2922c74cfb159e256e00b095f8053
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-01 20:08:38 +0530

    Comment out original generate_alert function
    Comment out the original generate_alert function for user monitoring.

--- alert.py
+++ alert.py
@@ -1,4 +1,4 @@
-def generate_alert(event_type, file_path):
-    print(f"[ALERT] {event_type} -> {file_path}")
+#def generate_alert(event_type, file_path): #user for monitoring senstive file tranfer
+    #print(f"[ALERT] {event_type} -> {file_path}")
 def generate_alert(alert_type, file_path):
     print(f"[ALERT] {alert_type} -> {file_path}")

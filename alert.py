ec65d19d5b49ce9d9c992bd43e60a3512e206caf
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-03 20:01:17 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,2 +1,2 @@
-def generate_alert(event_type, file_path):
-    print(f"[ALERT] {event_type} -> {file_path}")
+def generate_alert(alert_type, file_path):
+    print(f"[ALERT] {alert_type} -> {file_path}")

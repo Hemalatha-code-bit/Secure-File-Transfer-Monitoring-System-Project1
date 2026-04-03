11e076d5cee8594d6b068a0396eb4c87deb3589d
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-03 20:19:56 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -4,5 +4,5 @@ def generate_alert(alert_type, file_path):
     # Exact required format
     print(f"[ALERT] {alert_type} -> {file_path}")
 
-    # Also log it (important)
+    # Also log it 
     log_event(alert_type, file_path, "HIGH")

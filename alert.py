2d17e22f7ae16890cdb4c0b74a0c03f799a3d27e
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-03 20:29:13 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,8 +1,7 @@
 from logger import log_event
 
-def generate_alert(alert_type, file_path):
-    # Exact required format
-    print(f"[ALERT] {alert_type} -> {file_path}")
+def generate_alert(alert_type, file_path, severity="HIGH"):
+    print(f"[ALERT] {severity} | {alert_type} -> {file_path}")
 
-    # Also log it 
-    log_event(alert_type, file_path, "HIGH")
+    # Log alert also
+    log_event(alert_type, file_path, severity)

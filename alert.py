599f77b53571cde98e0d6be07dd08036941de181
Hemalatha-code-bit <shettigar1995@gmail.com>
2026-04-06 12:16:42 +0530

    Refactor generate_alert function parameters
    Removed severity parameter and logging from generate_alert function.

--- alert.py
+++ alert.py
@@ -1,7 +1,2 @@
-from logger import log_event
-
-def generate_alert(alert_type, file_path, severity="HIGH"):
-    print(f"[ALERT] {severity} | {alert_type} -> {file_path}")
-
-    # Log alert also
-    log_event(alert_type, file_path, severity)
+def generate_alert(event_type, file_path):
+    print(f"[ALERT] {event_type} -> {file_path}")
